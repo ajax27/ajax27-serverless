77b37b257940dfee3194cde193cb54eee89c3911
Shaun Collins <shaun.c16@hotmail.co.uk>
2020-06-09 14:45:11 +0100

    Added logic for updateting Airtable table and error messages

---
+++ src/styled/HighScores.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ScoresList = styled.ol`
+  padding-left: 24px;
+  ${'' /* list-style: none; */}
+  padding-bottom: 5px;
+`;
+
+export const ScoreLi = styled.li`
+  font-size: 1.6rem;
+  font-weight: 500;
+  letter-spacing: 1px;
+  color: #000;
+  text-shadow: 1px 1px 1px blue;
+  margin-bottom: 0.7rem;
+`;
